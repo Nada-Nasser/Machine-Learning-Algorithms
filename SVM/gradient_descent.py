016aaece88c940d451aaf637f5304cbfae817cc3
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2021-01-02 02:59:58 +0200

    SVM

--- SVM/gradient_descent.py
+++ SVM/gradient_descent.py
@@ -2,31 +2,36 @@ import numpy as np
 
 
 def hypothesis(x, theta):
-    return theta * x
+    f = np.sum(theta.T * x, axis=1)
+    hx = f.apply(lambda v: 1 if v >= 0 else 0)
+    return hx
 
 
 def compute_cost(x, y, theta):
-    fx = np.sum(hypothesis(x, theta), axis=1)
-    temp = y * fx
-    cost = temp.apply(lambda v: 0 if v >= 1 else max(0, 1-v))
+    fx = y * hypothesis(x, theta)
+    cost = np.sum(fx.apply(lambda v: 0 if v >= 1 else max(0, 1 - v)))
     return cost
 
 
-def gradient_descent(x, y, theta, alpha, n_iterations):
+def gradient_descent(x, y, theta, alpha, lmda, n_iterations):
     m = len(x)  # m = size of the training data
-    n_theta = 3
     cost = np.zeros(n_iterations)  # list to store the cost in every iteration,
-    # initialize it with zeros # list to store the cost in every iteration,
-    # initialize it with zeros
 
     for iteration in range(n_iterations):  # start the algorithm
+        o=0
+        u=0
+        hx = hypothesis(x, theta)
 
-        hx = np.sum(hypothesis(x, theta), axis=1)
-        temp = y * hx
+        for i in range(m):
+            if y[i] == hx[i]:
+                theta = np.array(theta - (alpha * ((2 * lmda) * theta)))
+                o = o+1
+            else:
+                u = u+1
+                theta = np.array(theta + (alpha * ((y[i] * x.iloc[i, :]) - ((2 * lmda) * theta))))
 
-        # for j in range(n_theta):  # for each theta j, update its value
-        #     theta[j] = theta[j] - (alpha / m) * sum((hx - y) * x.iloc[:, j])
-        #
-        # cost[iteration] = (compute_cost(x, y, theta))
-
+        cost[iteration] = np.sum(compute_cost(x, y, theta))
+        print(theta)
+    print(o)
+    print(u)
     return theta, cost
--- SVM/svm.py
+++ SVM/svm.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 
 from SVM.gradient_descent import *
 
-input_fields = ['age', 'trestbps']
+input_fields = ['age', 'cp']
 output_fields = ['target']
 
 path = "heart.csv"
 
 file_data = pd.read_csv(path, skipinitialspace=True)
-input_data = file_data[input_fields].head(100)
-output_data = file_data[output_fields].head(100)
+input_data = file_data[input_fields].head(300)
+output_data = file_data[output_fields].head(300)
 
 color = file_data[output_fields[0]].apply(lambda x: 'red' if x == 0 else 'green')
 file_data.plot(kind='scatter', x=input_fields[0], y=input_fields[1], c=color)
@@ -29,18 +29,17 @@ y = output_data.iloc[:, 0]  # all rows x last column
 # print(y)
 
 w = np.array([0] * (len(x.columns)))
+
 # print(w)
-alpha = 0.1
-n_iterations = 100
+alpha = 0.111
+n_iterations = 10
+lmda = 0.111
 
-# print(y * np.sum(hypothesis(x, w), axis=1))
-#
-# print("COST = ")
-# print(compute_cost(x, y, w))
-
-optimized_w, cost = gradient_descent(x, y, w, alpha, n_iterations)
-
+optimized_w, cost = gradient_descent(x, y, w, alpha, lmda, n_iterations)
 
 print("Optimized THETA : ")
 print(optimized_w)
-#
+print(cost)
+
+
+print(hypothesis(x, optimized_w))
