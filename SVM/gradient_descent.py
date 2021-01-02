6a650d5ad429400b8d6638beee825913fd44cdb9
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2021-01-02 22:29:14 +0200

    SVM

--- SVM/gradient_descent.py
+++ SVM/gradient_descent.py
@@ -2,14 +2,14 @@ import numpy as np
 
 
 def hypothesis(x, theta):
-    f = np.sum(theta.T * x, axis=1)
-    hx = f.apply(lambda v: 1 if v >= 0 else 0)
-    return hx
+    fx = np.sum(theta.T * x, axis=1)
+    hx = fx.apply(lambda v: 1 if v >= 1 else -1)
+    return hx, fx
 
 
 def compute_cost(x, y, theta):
-    fx = y * hypothesis(x, theta)
-    cost = np.sum(fx.apply(lambda v: 0 if v >= 1 else max(0, 1 - v)))
+    hx, fx = hypothesis(x, theta)
+    cost = (y*fx).apply(lambda v: 0 if v >= 1 else max(0, 1 - v))
     return cost
 
 
@@ -18,20 +18,14 @@ def gradient_descent(x, y, theta, alpha, lmda, n_iterations):
     cost = np.zeros(n_iterations)  # list to store the cost in every iteration,
 
     for iteration in range(n_iterations):  # start the algorithm
-        o=0
-        u=0
-        hx = hypothesis(x, theta)
-
+        hx, fx = hypothesis(x, theta)
         for i in range(m):
-            if y[i] == hx[i]:
+            if y[i] * fx[i] >= 1:
                 theta = np.array(theta - (alpha * ((2 * lmda) * theta)))
-                o = o+1
             else:
-                u = u+1
                 theta = np.array(theta + (alpha * ((y[i] * x.iloc[i, :]) - ((2 * lmda) * theta))))
 
-        cost[iteration] = np.sum(compute_cost(x, y, theta))
-        print(theta)
-    print(o)
-    print(u)
+        c = np.sum(compute_cost(x, y, theta))
+        lmda = 1.0/c
+        cost[iteration] = c
     return theta, cost
--- SVM/svm.py
+++ SVM/svm.py
@@ -1,45 +1,76 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D
 from SVM.gradient_descent import *
 
-input_fields = ['age', 'cp']
-output_fields = ['target']
+# input_fields = ['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']
 
-path = "heart.csv"
+input_fields = ['trestbps', 'chol','thalach', 'oldpeak']
+output_fields = ['target']
+path = "training_data.csv"
 
 file_data = pd.read_csv(path, skipinitialspace=True)
-input_data = file_data[input_fields].head(300)
-output_data = file_data[output_fields].head(300)
+input_data = file_data[input_fields].head(100)
+output_data = file_data[output_fields].head(100)
 
-color = file_data[output_fields[0]].apply(lambda x: 'red' if x == 0 else 'green')
-file_data.plot(kind='scatter', x=input_fields[0], y=input_fields[1], c=color)
-# plt.show()
+
+# rescaling data
+input_data = (input_data - input_data.mean()) / input_data.std()
 
 # add ones column
 input_data.insert(2, 'Ones', 1)
 
-# print(input_data)
-
 x = input_data.iloc[:, :]  # all rows x all columns expect the last one
 y = output_data.iloc[:, 0]  # all rows x last column
+y = y.apply(lambda v: -1 if v < 1 else 1)
+w = np.array([0] * (len(x.columns))) # [ 0 0 0]
 
-# print(x)
-# print(y)
 
-w = np.array([0] * (len(x.columns)))
+alpha = 0.0001
+n_iterations = 500
+lmda = 0
 
-# print(w)
-alpha = 0.111
-n_iterations = 10
-lmda = 0.111
+print(w)
 
-optimized_w, cost = gradient_descent(x, y, w, alpha, lmda, n_iterations)
+w, cost = gradient_descent(x, y, w, alpha, lmda, n_iterations)
 
 print("Optimized THETA : ")
-print(optimized_w)
+print(w)
 print(cost)
 
+## draw error graph
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.plot(np.arange(n_iterations), cost, 'r')
+ax.set_xlabel('Iterations')
+ax.set_ylabel('Cost')
+ax.set_title('total cost graph')
 
-print(hypothesis(x, optimized_w))
+
+hx, fx = hypothesis(x, w)
+
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(111, projection='3d')
+xx = input_data.iloc[:, 1]
+yy = input_data.iloc[:, 2]
+zz = input_data.iloc[:, 3]
+c = output_data.iloc[:, -1]
+img = ax.scatter(xx, yy, zz, c = c)
+fig.colorbar(img)
+plt.title("Original Output")
+
+
+fig2 = plt.figure(figsize=(5, 5))
+ax = fig2.add_subplot(111, projection='3d')
+xx = input_data.iloc[:, 1]
+yy = input_data.iloc[:, 2]
+zz = input_data.iloc[:, 3]
+c = hx.apply(lambda v: 0 if v < 1 else 1)
+img2 = ax.scatter(xx,yy,zz,c = c)
+fig2.colorbar(img2)
+plt.title("predicted Output")
+
+
+print(hx)
+
+plt.show()
